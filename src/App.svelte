1d8b4491ccd1e3e1e4669fd103e4a0141ee7fd8a
Sorami Hisamoto <s@89.io>
2023-04-25 21:02:03 +0900

    🙀

---
+++ src/App.svelte
@@ -0,0 +1,103 @@
+<script>
+  import { onMount } from "svelte";
+
+  const commentCandidates = [
+    "なんだって！？",
+    "衝撃の事実！！",
+    "信じられない！！",
+    "ジンギスカン食べたい！",
+  ];
+  let comment =
+    commentCandidates[Math.floor(Math.random() * commentCandidates.length)];
+
+  const loadImage = (canvas) => {
+    return new Promise((resolve, reject) => {
+      const image = new Image();
+      image.src = "/cat.png";
+      image.onload = () => {
+        let ctx = canvas.getContext("2d");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0);
+        resolve();
+      };
+      image.onerror = reject;
+    });
+  };
+
+  const drawText = (canvas, text) => {
+    let ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "rgb(255, 255, 255)";
+    ctx.fillRect(0, 0, canvas.width, 110);
+
+    ctx.font = "bold 42px sans-serif";
+    ctx.fillStyle = "rgb(234, 88, 12)";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(text, canvas.width / 2, 60);
+  };
+
+  $: {
+    let canvas = document.getElementById("cat");
+    if (canvas) drawText(canvas, comment);
+  }
+
+  const download = () => {
+    let canvas = document.getElementById("cat");
+    const a = document.createElement("a");
+    a.href = canvas.toDataURL("image/png");
+    a.download = "suzuki-cat.png";
+    a.click();
+  };
+
+  const copy = () => {
+    let canvas = document.getElementById("cat");
+    canvas.toBlob((blob) => {
+      const item = new ClipboardItem({ "image/png": blob });
+      navigator.clipboard.write([item]);
+    });
+  };
+
+  onMount(async () => {
+    const canvas = document.getElementById("cat");
+    await loadImage(canvas);
+    drawText(canvas, comment);
+  });
+</script>
+
+<main class="max-w-4xl m-auto my-1 p-1 flex h-screen">
+  <div class="m-auto">
+    <div class="flex flex-col items-center gap-y-4">
+      <div class="font-bold text-2xl text-slate-600 font-mono">
+        Suzuki Cat Generator
+      </div>
+      <div class="">
+        <canvas id="cat" />
+      </div>
+      <div class="mt-2">
+        <textarea
+          bind:value={comment}
+          name="comment"
+          id="comment"
+          rows="1"
+          class="block w-72 rounded p-3 text-orange-600 font-bold shadow-sm border-1.5 border-gray-700"
+        />
+      </div>
+      <div class="text-sm flex gap-x-2">
+        <button
+          type="button"
+          on:click={copy}
+          class="rounded-md bg-gray-700 px-3 py-1 shadow text-white hover:bg-orange-600"
+          >クリックボードへコピー</button
+        >
+        <button
+          type="button"
+          on:click={download}
+          class="rounded-md bg-gray-700 px-3 py-1 shadow text-white hover:bg-orange-600"
+          >ダウンロード</button
+        >
+      </div>
+    </div>
+  </div>
+</main>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "@unocss/reset/tailwind-compat.css";
+import "virtual:uno.css";
+import "./app.css";
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
